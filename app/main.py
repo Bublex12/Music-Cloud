70fe1d9e70f608fa8120c3c9e7efa9e16a6ae8ac
Bublex12 <gazizovdanil@gmail.com>
2023-07-26 13:06:13 +0500

    Test_commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,32 @@
-
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
 
-    return HTMLResponse("<h2>Hello METANIT.COM</h2>")
+# Получить список всех аудиофайлов в папке audio
+def get_audio_files():
+    audio_folder = "../static/audio"
+    audio_files = []
+    for filename in os.listdir(audio_folder):
+        if filename.endswith(".mp3"):
+            audio_files.append(filename)
+    return audio_files
+
+
+templates = Jinja2Templates(directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../templates"))
+
+
+@app.get("/audio", response_class=HTMLResponse)
+async def get_all_audio(request: Request):
+    audio_files = get_audio_files()
+    return templates.TemplateResponse("index.html", {"request": request, "audio_files": audio_files})
+
+
+@app.get("/audio/{filename}", response_class=FileResponse)
+async def get_audio(filename: str):
+    audio_folder = "../static/audio"
+    audio_path = os.path.join(audio_folder, filename)
+    return audio_path
